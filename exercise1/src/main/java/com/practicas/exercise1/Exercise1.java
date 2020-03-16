0d8acefb58553487ba6125685d2580a0d53b80d5
darconoje <dcondeojeda@gmail.com>
2020-03-16 12:49:31 +0100

    Actualizado

--- exercise1/src/main/java/com/practicas/exercise1/Exercise1.java
+++ exercise1/src/main/java/com/practicas/exercise1/Exercise1.java
@@ -6,15 +6,21 @@ import com.practicas.exercise1.utils.DatabaseJson;
 
 public class Exercise1 {
 
-    public static void main(String[] args){
-    	
-    	JSONArray array = DatabaseJson.loadDatabase().getData();
-    	
-    	funcionesEjercicio1 funciones = new funcionesEjercicio1();
-    	
-    	funciones.devolverMarcayModelo(array);
-    }
-    
+ public static void main(String[] args) {
+
+  //se crea array de coches a partir del archivo json	 
+  JSONArray array = DatabaseJson.loadDatabase().getData();
+
+  //se crea objeto funciones usado para llamar a los metodos
+  FuncionesEjercicio1 funciones = new FuncionesEjercicio1();
+  System.out.println("Funcion 1:");
+  //Funcion 1 - Devuelve marca y modelo de los coches dentro del rango de los limites indicados
+  funciones.devolverMarcayModelo(array, 24, 32);
+  System.out.println("Funcion 2:");
+  //Funcion 2 - Devuelve los primeros n coches del array de coches que superen la potencia indicada
+  funciones.primerosPotencia(array, 10, 150);
+ }
+
 }
 
 
--- exercise1/src/main/java/com/practicas/exercise1/funcionesEjercicio1.java
+++ exercise1/src/main/java/com/practicas/exercise1/funcionesEjercicio1.java
@@ -3,14 +3,29 @@ package com.practicas.exercise1;
 import org.json.JSONArray;
 import org.json.JSONObject;
 
-public class funcionesEjercicio1 {
+public class FuncionesEjercicio1 {
 
-    public void devolverMarcayModelo(JSONArray array) {	 
-    	
-    	for( int i = 23 ; i<=31 ; i++ ) {
-    		System.out.println("Coche " + (i+1) + " - Marca: " + ((JSONObject)array.get(i)).getJSONObject("Identification").getString("Make") +" Modelo: " + ((JSONObject)array.get(i)).getJSONObject("Identification").getString("Model Year") );
-    	}
-    	
-    }
-	
+ //Funcion 1 - Devuelve marca y modelo de los coches dentro del rango de los limites indicados	
+ public void devolverMarcayModelo(JSONArray array, int liminferior, int limsuperior) {
+
+  for (int i = liminferior - 1; i <= limsuperior - 1; i++) {
+   System.out.println("Coche " + (i + 1) + " - Marca: " + ((JSONObject) array.get(i)).getJSONObject("Identification").getString("Make") + " ; Modelo: " + ((JSONObject) array.get(i)).getJSONObject("Identification").getString("ID"));
+  }
+
+ }
+
+ //Funcion 2 - Devuelve los primeros n coches del array de coches que superen la potencia indicada
+ public void primerosPotencia(JSONArray array, int n, int potencia) {
+  //se inicia el contador a 1
+  int contador = 1;
+  //se recorre el array de coches
+  for (int i = 0; i < array.length(); i++) {
+   //si el contador es menor o igual al n indicado y la potencia es mayor o igual que la indicada recoge el modelo y marca del coche y lo imprime
+   if (((JSONObject) array.get(i)).getJSONObject("Engine Information").getJSONObject("Engine Statistics").getInt("Horsepower") >= potencia && contador <= n) {
+    System.out.println("Coche " + (i + 1) + " - Marca: " + ((JSONObject) array.get(i)).getJSONObject("Identification").getString("Make") + " ; Modelo: " + ((JSONObject) array.get(i)).getJSONObject("Identification").getString("ID"));
+    contador++;
+   }
+  }
+ }
+
 }
